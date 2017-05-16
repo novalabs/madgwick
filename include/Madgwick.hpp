75e7d42b4bed98e99648c8d1524b4608185f0e3e
Davide Rizzi <d.rizzi@novalabs.io>
2017-05-16 14:29:38 +0200

    ...

--- include/Madgwick.hpp
+++ include/Madgwick.hpp
@@ -20,53 +20,53 @@
 namespace core {
 namespace madgwick {
 class Madgwick:
-   public core::mw::CoreNode,
-   public core::mw::CoreConfigurable<core::madgwick::MadgwickConfiguration>
+    public core::mw::CoreNode,
+    public core::mw::CoreConfigurable<core::madgwick::MadgwickConfiguration>
 {
 public:
-   Madgwick(
-      const char*                name,
-      core::os::Thread::Priority priority = core::os::Thread::PriorityEnum::NORMAL
-   );
-   virtual
-   ~Madgwick();
+    Madgwick(
+        const char*                name,
+        core::os::Thread::Priority priority = core::os::Thread::PriorityEnum::NORMAL
+    );
+    virtual
+    ~Madgwick();
 
 private:
-   ModuleConfiguration::L3GD20H_GYRO_DATATYPE _gyroData;
-   ModuleConfiguration::LSM303D_ACC_DATATYPE  _accData;
-   ModuleConfiguration::LSM303D_MAG_DATATYPE  _magData;
+    ModuleConfiguration::L3GD20H_GYRO_DATATYPE _gyroData;
+    ModuleConfiguration::LSM303D_ACC_DATATYPE  _accData;
+    ModuleConfiguration::LSM303D_MAG_DATATYPE  _magData;
 
 private:
-   bool
-   onPrepareMW();
+    bool
+    onPrepareMW();
 
-   bool
-   onLoop();
+    bool
+    onLoop();
 
-   static bool
-   gyroCallback(
-      const ModuleConfiguration::L3GD20H_GYRO_DATATYPE& msg,
-      void*                                             context
-   );
+    static bool
+    gyroCallback(
+        const ModuleConfiguration::L3GD20H_GYRO_DATATYPE& msg,
+        void*                                             context
+    );
 
-   static bool
-   accCallback(
-      const ModuleConfiguration::LSM303D_ACC_DATATYPE& msg,
-      void*                                            context
-   );
+    static bool
+    accCallback(
+        const ModuleConfiguration::LSM303D_ACC_DATATYPE& msg,
+        void*                                            context
+    );
 
-   static bool
-   magCallback(
-      const ModuleConfiguration::LSM303D_MAG_DATATYPE& msg,
-      void*                                            context
-   );
+    static bool
+    magCallback(
+        const ModuleConfiguration::LSM303D_MAG_DATATYPE& msg,
+        void*                                            context
+    );
 
 
 private:
-   core::mw::Subscriber<ModuleConfiguration::L3GD20H_GYRO_DATATYPE, 2> _subscriberGyro;
-   core::mw::Subscriber<ModuleConfiguration::LSM303D_ACC_DATATYPE, 2>  _subscriberAcc;
-   core::mw::Subscriber<ModuleConfiguration::LSM303D_MAG_DATATYPE, 2>  _subscriberMag;
-   core::mw::Publisher<sensor_msgs::RPY_f32> _publisher;
+    core::mw::Subscriber<ModuleConfiguration::L3GD20H_GYRO_DATATYPE, 2> _subscriberGyro;
+    core::mw::Subscriber<ModuleConfiguration::LSM303D_ACC_DATATYPE, 2>  _subscriberAcc;
+    core::mw::Subscriber<ModuleConfiguration::LSM303D_MAG_DATATYPE, 2>  _subscriberMag;
+    core::mw::Publisher<sensor_msgs::RPY_f32> _publisher;
 };
 }
 }
--- src/Madgwick.cpp
+++ src/Madgwick.cpp
@@ -19,34 +19,34 @@
 namespace core {
 namespace madgwick {
 Madgwick::Madgwick(
-   const char*                name,
-   core::os::Thread::Priority priority
+    const char*                name,
+    core::os::Thread::Priority priority
 ) :
-   CoreNode::CoreNode(name, priority),
-   CoreConfigurable<core::madgwick::MadgwickConfiguration>::CoreConfigurable(name)
+    CoreNode::CoreNode(name, priority),
+    CoreConfigurable<core::madgwick::MadgwickConfiguration>::CoreConfigurable(name)
 {
-   _workingAreaSize = 512;
+    _workingAreaSize = 512;
 }
 
 Madgwick::~Madgwick()
 {
-   teardown();
+    teardown();
 }
 
 bool
 Madgwick::onPrepareMW()
 {
-   _subscriberGyro.set_callback(Madgwick::gyroCallback);
-   _subscriberAcc.set_callback(Madgwick::accCallback);
-   _subscriberMag.set_callback(Madgwick::magCallback);
+    _subscriberGyro.set_callback(Madgwick::gyroCallback);
+    _subscriberAcc.set_callback(Madgwick::accCallback);
+    _subscriberMag.set_callback(Madgwick::magCallback);
 
-   this->subscribe(_subscriberGyro, configuration().topicGyro);
-   this->subscribe(_subscriberAcc, configuration().topicAcc);
-   this->subscribe(_subscriberMag, configuration().topicMag);
+    this->subscribe(_subscriberGyro, configuration().topicGyro);
+    this->subscribe(_subscriberAcc, configuration().topicAcc);
+    this->subscribe(_subscriberMag, configuration().topicMag);
 
-   this->advertise(_publisher, configuration().topic);
+    this->advertise(_publisher, configuration().topic);
 
-   return true;
+    return true;
 }
 
 bool
@@ -54,103 +54,103 @@ Madgwick::onLoop()
 {
 //      core::os::Time t = core::os::Time::now();
 
-   while (mustLoop()) {
-      // Override the CoreNode loop...
-      if (core::os::Thread::should_terminate()) {
-         teardown();
-      } else {
-         /*
-            if (!this->spin(Configuration::SUBSCRIBER_SPIN_TIME)) {
-            Module::led.toggle();
-            } else {
-          */
-         core::common_msgs::Vector3_i16 acc_msg;
-         core::common_msgs::Vector3_i16 gyro_msg;
+    while (mustLoop()) {
+        // Override the CoreNode loop...
+        if (core::os::Thread::should_terminate()) {
+            teardown();
+        } else {
+            /*
+               if (!this->spin(Configuration::SUBSCRIBER_SPIN_TIME)) {
+               Module::led.toggle();
+               } else {
+             */
+            core::common_msgs::Vector3_i16 acc_msg;
+            core::common_msgs::Vector3_i16 gyro_msg;
 
-         systime_t time;
-         time = chVTGetSystemTime();
+            systime_t time;
+            time = chVTGetSystemTime();
 
-         Module::acc.get(acc_msg);
-         Module::gyro.get(gyro_msg);
+            Module::acc.get(acc_msg);
+            Module::gyro.get(gyro_msg);
 
-         // --- LOOP BEGIN -----------------------------------------------------
-         MadgwickAHRSupdateIMU(core::utils::math::conversions::Deg2Rad<float>(gyro_msg.y / 57.143f),
-                               core::utils::math::conversions::Deg2Rad<float>(-gyro_msg.x / 57.143f),
-                               core::utils::math::conversions::Deg2Rad<float>(gyro_msg.z / 57.143f),
-                               acc_msg.x / 1000.0,
-                               acc_msg.y / 1000.0,
-                               acc_msg.z / 1000.0);
+            // --- LOOP BEGIN -----------------------------------------------------
+            MadgwickAHRSupdateIMU(core::utils::math::conversions::Deg2Rad<float>(gyro_msg.y / 57.143f),
+                                  core::utils::math::conversions::Deg2Rad<float>(-gyro_msg.x / 57.143f),
+                                  core::utils::math::conversions::Deg2Rad<float>(gyro_msg.z / 57.143f),
+                                  acc_msg.x / 1000.0,
+                                  acc_msg.y / 1000.0,
+                                  acc_msg.z / 1000.0);
 
-         attitude_t attitude;
+            attitude_t attitude;
 
-         getMadAttitude(&attitude);
+            getMadAttitude(&attitude);
 
-         sensor_msgs::RPY_f32* msgp;
+            sensor_msgs::RPY_f32* msgp;
 
-         if (_publisher.alloc(msgp)) {
-            msgp->roll  = core::utils::math::conversions::Rad2Deg<float>(attitude.roll);
-            msgp->pitch = core::utils::math::conversions::Rad2Deg<float>(attitude.pitch);
-            msgp->yaw   = core::utils::math::conversions::Rad2Deg<float>(attitude.yaw);
+            if (_publisher.alloc(msgp)) {
+                msgp->roll  = core::utils::math::conversions::Rad2Deg<float>(attitude.roll);
+                msgp->pitch = core::utils::math::conversions::Rad2Deg<float>(attitude.pitch);
+                msgp->yaw   = core::utils::math::conversions::Rad2Deg<float>(attitude.yaw);
 
-            _publisher.publish(*msgp);
-         }
-
-         /*
+                _publisher.publish(*msgp);
             }
-          */
-         /* Broken...
-            t = t + core::os::Time::ms((core::os::Time::Type)(1000.0f / configuration.frequency));
-            //core::os::Thread::sleep_until(t);
-            core::os::Thread::sleep(core::os::Time::ms(20));
-          */
 
-         time += MS2ST(20);
-         chThdSleepUntil(time);
+            /*
+               }
+             */
+            /* Broken...
+               t = t + core::os::Time::ms((core::os::Time::Type)(1000.0f / configuration.frequency));
+               //core::os::Thread::sleep_until(t);
+               core::os::Thread::sleep(core::os::Time::ms(20));
+             */
 
-         // --- LOOP END -------------------------------------------------------
-      }
-   }
+            time += MS2ST(20);
+            chThdSleepUntil(time);
 
-   return true;
+            // --- LOOP END -------------------------------------------------------
+        }
+    }
+
+    return true;
 }       // Madgwick::onLoop
 
 bool
 Madgwick::gyroCallback(
-   const ModuleConfiguration::L3GD20H_GYRO_DATATYPE& msg,
-   void*                                             context
+    const ModuleConfiguration::L3GD20H_GYRO_DATATYPE& msg,
+    void*                                             context
 )
 {
-   Madgwick* tmp = static_cast<Madgwick*>(context);
+    Madgwick* tmp = static_cast<Madgwick*>(context);
 
-   tmp->_gyroData = msg;
+    tmp->_gyroData = msg;
 
-   return true;
+    return true;
 }
 
 bool
 Madgwick::accCallback(
-   const ModuleConfiguration::LSM303D_ACC_DATATYPE& msg,
-   void*                                            context
+    const ModuleConfiguration::LSM303D_ACC_DATATYPE& msg,
+    void*                                            context
 )
 {
-   Madgwick* tmp = static_cast<Madgwick*>(context);
+    Madgwick* tmp = static_cast<Madgwick*>(context);
 
-   tmp->_accData = msg;
+    tmp->_accData = msg;
 
-   return true;
+    return true;
 }
 
 bool
 Madgwick::magCallback(
-   const ModuleConfiguration::LSM303D_MAG_DATATYPE& msg,
-   void*                                            context
+    const ModuleConfiguration::LSM303D_MAG_DATATYPE& msg,
+    void*                                            context
 )
 {
-   Madgwick* tmp = static_cast<Madgwick*>(context);
+    Madgwick* tmp = static_cast<Madgwick*>(context);
 
-   tmp->_magData = msg;
+    tmp->_magData = msg;
 
-   return true;
+    return true;
 }
 }
 }
